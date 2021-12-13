aa01db64d670b6130fb409be04f6567468edc21f
dschaub95 <schaub.darius@gmail.com>
2021-12-13 18:30:43 +0100

    updated requirements

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,5 @@
 conda install -y pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
-conda install -y numpy scipy cython tqdm scikit-learn matplotlib seaborn tensorboard pandas
+conda install -y numpy scipy cython tqdm scikit-learn matplotlib seaborn tensorboard pandas networkx
 conda install -y jupyterlab -c conda-forge
 pip install tensorboard_logger
 pip install wandb
