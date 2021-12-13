eec993fcf76ad598203fd85a88301976c495cb62
dschaub95 <schaub.darius@gmail.com>
2021-12-13 18:44:23 +0100

    created conda initialization file

--- init_conda_env.sh
+++ init_conda_env.sh
@@ -1,6 +1,7 @@
+conda create -n rltsp python=3.7
+conda activate rltsp
 conda install -y pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 conda install -y numpy scipy cython tqdm scikit-learn matplotlib seaborn tensorboard pandas networkx
 conda install -y jupyterlab -c conda-forge
 pip install tensorboard_logger
 pip install wandb
-pip install gdown
