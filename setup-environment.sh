a996c3e09eea0aeb983384023324324e5997f431
dschaub95 <schaub.darius@gmail.com>
2021-12-13 19:03:08 +0100

    fixed env setup

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -l
-conda create -y --name rltsp python=3.7
-conda activate rltsp
+# conda create -y --name rltsp python=3.7
+# conda activate rltsp
 conda install -y pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 conda install -y numpy scipy cython tqdm scikit-learn matplotlib seaborn tensorboard pandas networkx
 conda install -y jupyterlab -c conda-forge
