5ade48b421017f55bc6994446bf4226510788207
dschaub95 <schaub.darius@gmail.com>
2021-12-21 23:42:42 +0100

    updated test script

--- scripts/test_multiple.sh
+++ scripts/test_multiple.sh
@@ -8,8 +8,8 @@ echo Select CUDA_VISIBLE_DEVICES
 read cuda_device
 
 # run all the tests
-CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100
 CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 --use_pomo_augmentation
-CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 --sampling_steps=32
-CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 --sampling_steps=16
-CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 --sampling_steps=8
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 --use_pomo_augmentation --num_nodes=20
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 --use_pomo_augmentation --num_nodes=50
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 --use_pomo_augmentation --num_nodes=200
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 --use_pomo_augmentation --num_nodes=500
