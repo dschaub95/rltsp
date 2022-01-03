8dee2a29cae6aec6008ad14fef1a4e92d1587303
dschaub95 <schaub.darius@gmail.com>
2022-01-03 21:13:31 +0100

    updated env setup

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash -l
 # conda create -y --name rltsp python=3.7
 # conda activate rltsp
-conda install -y pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+conda install -y pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+conda install -y pyg -c pyg -c conda-forge
 conda install -y numpy scipy cython tqdm scikit-learn matplotlib seaborn tensorboard pandas networkx
 conda install -y jupyterlab -c conda-forge
 pip install tensorboard_logger
 pip install wandb
+pip install tsplib95
