5dd7e8fbd66c1b2b1146f9cbc519519fe0a62378
dschaub95 <schaub.darius@gmail.com>
2022-01-04 23:21:39 +0100

    first env preparations for mcts

--- main_code/environment/environment_new.py
+++ main_code/environment/environment_new.py
@@ -43,10 +43,10 @@ class GroupState:
         # size = (batch, group)
         return group_travel_distances
 
-    def move_to(self, selected_idx_mat):
+    def transition(self, selected_idx_mat):
         # selected_idx_mat.shape = (batch, group)
 
-        # History
+        # History update
         ####################################
         self.selected_count += 1
         self.current_node = selected_idx_mat
@@ -61,8 +61,8 @@ class GroupState:
     @property
     def available_actions(self):
         # return all currently available actions
-        pass
-    
+        full_node_list = LongTensor(np.arange(self.tsp_size))[None, None, :].expand(self.batch_s, self.group_s, self.tsp_size)
+        full_node_list.gather(dim=-1, index=self.selected_node_list)
 
 class GroupEnvironment:
 
@@ -87,7 +87,7 @@ class GroupEnvironment:
         # selected_idx_mat.shape = (batch, group)
 
         # move state
-        self.group_state.move_to(selected_idx_mat)
+        self.group_state.transition(selected_idx_mat)
 
         # returning values
         self.done = (self.group_state.selected_count == self.tsp_size)
@@ -100,104 +100,20 @@ class GroupEnvironment:
     def _get_group_travel_distance(self):
         return self.group_state.compute_path_len()
     
+    @property
     def isdone(self):
         return (self.group_state.selected_count == self.tsp_size)
     
     @staticmethod
     def is_done_state(group_state: GroupState):
         return (group_state.selected_count == group_state.tsp_size)
+    
+    @staticmethod
+    def make_transition(group_state: GroupState, action):
+        group_state.transition(action)
+        return group_state
 
     @staticmethod
     def get_return(group_state: GroupState):
         assert group_state.selected_count == group_state.tsp_size
-        return group_state.compute_path_len()
-
-class State:
-    def __init__(self, seq, tsp_size):
-        self.seq = seq
-        self.batch_s = seq.size(0)
-
-        self.current_node = None
-        self.tsp_size = tsp_size
-        # History
-        ####################################
-        self.selected_count = 0
-        self.available_mask = BoolTensor(np.ones((self.batch_s, self.tsp_size)))
-        self.ninf_mask = Tensor(np.zeros((self.batch_s, self.tsp_size)))
-        # shape = (batch_s, tsp_size)
-        self.selected_node_list = LongTensor(np.zeros((self.batch_s, 0)))
-        # shape = (batch_s, selected_count)
-
-    def move_to(self, selected_node_idx):
-        # selected_node_idx.shape = (batch,)
-
-        self.current_node = selected_node_idx
-
-        # History
-        ####################################
-        self.selected_count += 1
-        self.available_mask[torch.arange(self.batch_s), selected_node_idx] = False
-        self.ninf_mask[torch.arange(self.batch_s), selected_node_idx] = -np.inf
-        self.selected_node_list = torch.cat((self.selected_node_list, selected_node_idx[:, None]), dim=1)
-
-class Environment:
-    def __init__(self, seq, tsp_size):
-        # seq.shape = (batch, tsp_size, 2)
-
-        self.seq = seq
-        self.batch_s = seq.size(0)
-        self.state = None
-        self.tsp_size = tsp_size
-
-    def reset(self):
-        self.state = State(self.seq)
-
-        reward = None
-        done = False
-        return self.state, reward, done
-
-    def step(self, selected_node_idx):
-        # selected_node_idx.shape = (batch,)
-
-        # move state
-        self.state.move_to(selected_node_idx)
-
-        # returning values
-        done = self.state.selected_count == self.tsp_size
-        if done:
-            reward = -self._get_travel_distance()  # note the minus sign!
-        else:
-            reward = None
-        return self.state, reward, done
-
-    def _get_travel_distance(self):
-
-        gathering_index = self.state.selected_node_list.unsqueeze(2).expand(-1, self.tsp_size, 2)
-        # shape = (batch, tsp_size, 2)
-        ordered_seq = self.seq.gather(dim=1, index=gathering_index)
-
-        rolled_seq = ordered_seq.roll(dims=1, shifts=-1)
-        segment_lengths = ((ordered_seq-rolled_seq)**2).sum(2).sqrt()
-        # size = (batch, tsp_size)
-
-        travel_distances = segment_lengths.sum(1)
-        # size = (batch,)
-        return travel_distances
-
-class BaseState:
-    def __init__(self) -> None:
-        pass
-
-class BaseEnvironment:
-    def __init__(self) -> None:
-        pass
-    
-    def step(self, action):
-        pass
-
-    def isdone(self, state):
-        pass
-
-class TSPEnvironment(BaseEnvironment):
-    def __init__(self) -> None:
-        super().__init__()
+        return -group_state.compute_path_len()
