45b18c8b25c1dbcf439698b1ecba8f1e3c393648
dschaub95 <schaub.darius@gmail.com>
2022-01-15 15:08:03 +0100

    test: add mcts test scripts

---
+++ scripts/test_mcts_playouts.sh
@@ -0,0 +1,41 @@
+echo Select CUDA_VISIBLE_DEVICES
+read cuda_device
+
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
+                                                 --test_set=uniform_n_20_128 \
+                                                 --test_batch_size=1024 \
+                                                 --use_mcts \
+                                                 --num_playouts=10 \
+                                                 --c_puct=5.0
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
+                                                 --test_set=uniform_n_20_128 \
+                                                 --test_batch_size=1024 \
+                                                 --use_mcts \
+                                                 --num_playouts=20 \
+                                                 --c_puct=5.0
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
+                                                 --test_set=uniform_n_20_128 \
+                                                 --test_batch_size=1024 \
+                                                 --use_mcts \
+                                                 --num_playouts=30 \
+                                                 --c_puct=5.0
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
+                                                 --test_set=uniform_n_20_128 \
+                                                 --test_batch_size=1024 \
+                                                 --use_mcts \
+                                                 --num_playouts=50 \
+                                                 --c_puct=5.0
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
+                                                 --test_set=uniform_n_20_128 \
+                                                 --test_batch_size=1024 \
+                                                 --use_mcts \
+                                                 --num_playouts=100 \
+                                                 --c_puct=5.0
+
+
+
