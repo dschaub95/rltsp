360a59eb63d782850d4ca7109ca0ae751079b91c
dschaub95 <schaub.darius@gmail.com>
2022-01-22 00:32:51 +0100

    feat: add multistate class for multi state batching

--- main_code/environment/environment_new.py
+++ main_code/environment/environment_new.py
@@ -87,6 +87,64 @@ class GroupState:
         return full_node_list[mask.bool()].view(self.batch_s, self.group_s, self.tsp_size - self.selected_count)
         # shape (batch_s, group_s, tsp_size - selected_count)
 
+    # @property
+    # def selected_count(self):
+
+class MultiGroupState(GroupState):
+    def __init__(self, state_list):
+        # combine states
+        data = torch.cat([state.data for state in state_list], dim=0)
+        group_size = state_list[0].group_s
+        tsp_size = state_list[0].tsp_size
+        self.single_batch_s = state_list[0].batch_s
+        self.num_single_states = len(state_list)
+        # sets the correct batch size
+        super().__init__(group_size, data, tsp_size)
+        # set selected count as vector to allow addition of ints
+        self.selected_count = np.array([state.selected_count for state in state_list])
+        max_count = np.max(self.selected_count)
+        # pad selected_node list tensors to the left
+        self.selected_node_list = torch.cat([self.pad3d_left(state.selected_node_list, max_count) for state in state_list], dim=0)
+        self.ninf_mask = torch.cat([state.ninf_mask for state in state_list], dim=0)
+        self.current_node = torch.cat([state.current_node for state in state_list], dim=0)
+
+    # overwrite available actions
+    def pad3d_left(self, tensor, desired_size: int):
+        orig_size = tensor.size(-1)
+        tmp = tensor[:,:,0][:,:,None].expand(-1, -1, desired_size).detach().clone()
+        tmp[:,:,-orig_size::] = tensor
+        return tmp
+    
+    # adding to selected count works for numpy array
+    def transition(self, selected_idx_mat):
+        return super().transition(selected_idx_mat)
+    
+    def compute_path_len(self):
+        # reduce to single state case
+        states = self.split()
+        return torch.cat([state.compute_path_len() for state in states], dim=0)
+    
+    def split(self):
+        # split multi group state into original single group states
+        orig_batch_s = self.single_batch_s
+        orig_data = self.data[0:orig_batch_s,:,:]
+        # orig_data.shape = (batch_s, tsp_size, 2)
+        states = []
+        for k, selected_count in enumerate(self.selected_count):
+            state = GroupState(self.group_s, data=orig_data, tsp_size=self.tsp_size)
+            state.selected_count = np.clip(selected_count, 0, self.tsp_size)
+            state.ninf_mask = self.ninf_mask[k*orig_batch_s:(k+1)*orig_batch_s,:,:]
+            # recover the true selected node tensor
+            raw_selected_node_list = self.selected_node_list[k*orig_batch_s:(k+1)*orig_batch_s,:,:]
+            num_selected_nodes = raw_selected_node_list.size(-1)
+            # recover single selected node list
+            state.selected_node_list = torch.unique_consecutive(raw_selected_node_list, dim=-1)[:,:,0:self.tsp_size]
+            # recover last selected or current node
+            state.current_node = state.selected_node_list[:,:,-1]
+            states.append(state)
+        return states
+    
+
 class GroupEnvironment:
     def __init__(self, data, tsp_size):
         # seq.shape = (batch, tsp_size, 2)
@@ -121,17 +179,17 @@ class GroupEnvironment:
 
         # returning values
         self.done = (self.group_state.selected_count == self.tsp_size)
-        if self.done:
+        if self.is_done:
             reward = -self._get_group_travel_distance()  # note the minus sign!
         else:
             reward = None
-        return self.group_state, reward, self.done
+        return self.group_state, reward, self.is_done
 
     def _get_group_travel_distance(self):
         return self.group_state.compute_path_len()
     
     @property
-    def isdone(self):
+    def is_done(self):
         return (self.group_state.selected_count == self.tsp_size)
     
     @staticmethod
