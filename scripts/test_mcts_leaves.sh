5c9af66299e9c76ecbe52090cdfabfdf8059786d
dschaub95 <schaub.darius@gmail.com>
2022-01-22 16:28:29 +0100

    test: adapt cpuct in test script

--- scripts/test_mcts_leaves.sh
+++ scripts/test_mcts_leaves.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --use_mcts \
                                                  --num_playouts=10 \
                                                  --num_parallel=1 \
-                                                 --c_puct=5.0
+                                                 --c_puct=7.5
 
 CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --test_set=uniform_n_20_128 \
@@ -15,7 +15,7 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --use_mcts \
                                                  --num_playouts=10 \
                                                  --num_parallel=2 \
-                                                 --c_puct=5.0
+                                                 --c_puct=7.5
 
 CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --test_set=uniform_n_20_128 \
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --use_mcts \
                                                  --num_playouts=10 \
                                                  --num_parallel=5 \
-                                                 --c_puct=5.0
+                                                 --c_puct=7.5
 
 CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --test_set=uniform_n_20_128 \
@@ -31,7 +31,7 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --use_mcts \
                                                  --num_playouts=10 \
                                                  --num_parallel=10 \
-                                                 --c_puct=5.0
+                                                 --c_puct=7.5
 
 CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --test_set=uniform_n_20_128 \
@@ -39,7 +39,7 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --use_mcts \
                                                  --num_playouts=10 \
                                                  --num_parallel=15 \
-                                                 --c_puct=5.0
+                                                 --c_puct=7.5
 
 CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --test_set=uniform_n_20_128 \
@@ -47,4 +47,4 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=1 \
                                                  --use_mcts \
                                                  --num_playouts=10 \
                                                  --num_parallel=20 \
-                                                 --c_puct=5.0
+                                                 --c_puct=7.5
