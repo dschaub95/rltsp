b142a7e363647d4807b968e28cd368d6ef73f14e
dschaub95 <schaub.darius@gmail.com>
2022-01-25 13:11:00 +0100

    test: mcts tsp100 test script update

--- scripts/test_mcts_tsp100.sh
+++ scripts/test_mcts_tsp100.sh
@@ -8,7 +8,7 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 \
                                                  --test_batch_size=1024 \
                                                  --use_mcts \
                                                  --num_playouts=10 \
-                                                 --num_parallel=2 \
+                                                 --num_parallel=1 \
                                                  --c_puct=7.5
 
 CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 \
@@ -16,8 +16,17 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 \
                                                  --test_set=uniform_n_100_128 \
                                                  --test_batch_size=1024 \
                                                  --use_mcts \
+                                                 --num_playouts=10 \
+                                                 --num_parallel=1 \
+                                                 --c_puct=10.0
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 \
+                                                 --use_pomo_aug \
+                                                 --test_set=uniform_n_100_128 \
+                                                 --test_batch_size=1024 \
+                                                 --use_mcts \
                                                  --num_playouts=20 \
-                                                 --num_parallel=2 \
+                                                 --num_parallel=1 \
                                                  --c_puct=7.5
 
 CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 \
@@ -26,7 +35,7 @@ CUDA_VISIBLE_DEVICES=$cuda_device python test.py --num_trajectories=100 \
                                                  --test_batch_size=1024 \
                                                  --use_mcts \
                                                  --num_playouts=30 \
-                                                 --num_parallel=2 \
+                                                 --num_parallel=1 \
                                                  --c_puct=7.5
 
 
