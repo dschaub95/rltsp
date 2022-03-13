66b9adfabe3b1ed9ff1ec2b4311296b3f9edb03d
dschaub95 <schaub.darius@gmail.com>
2022-03-13 16:49:15 +0100

    feat: add script for subsampling from dataset and add functions to source

---
+++ main_code/utils/data/utils.py
@@ -0,0 +1,62 @@
+import networkx as nx
+import numpy as np
+import random
+import torch
+import os
+import json
+from itertools import combinations
+
+# convert node coords into nx graph
+def convert_tsp_to_nx(node_coords):
+    num_nodes = node_coords.shape[0]
+    edges = [
+        (s[0], t[0], np.linalg.norm(s[1] - t[1]))
+        for s, t in combinations(enumerate(node_coords), 2)
+    ]
+    g = nx.Graph()
+    g.add_weighted_edges_from(edges)
+    feature_dict = {k: {"coord": node_coords[k]} for k in range(num_nodes)}
+    nx.set_node_attributes(g, feature_dict)
+    return g
+
+
+def calc_tour_length(node_feats, sol):
+    # calculate length of the tour
+    differences = node_feats[sol, :] - np.roll(node_feats[sol, :], shift=-1, axis=0)
+    summed_squares = np.sum(np.square(differences), axis=1)
+    length = np.sum(np.sqrt(summed_squares))
+    return length
+
+
+def save_tsp():
+    # generic code for saving tsp
+    # add to data utils
+    pass
+
+
+def sample_and_save_subset(dataset, save_path, seed=37):
+    random.seed(37)
+    indices = random.sample(range(len(dataset)), 128)
+    subset = torch.utils.data.Subset(dataset, indices)
+    # save data in new folder
+    # make sure length stays the same
+    for idx, data in enumerate(subset):
+        idx_str = f"{idx}".zfill(len(str(len(subset))))
+        problem_name = f"tsp_{idx_str}"
+        instance_path = f"{save_path}/{problem_name}"
+        if not os.path.exists(instance_path):
+            os.makedirs(instance_path)
+        node_feats = data[0]
+        np.savetxt(f"{instance_path}/node_feats.txt", node_feats)
+        sol = data[2]
+        length = calc_tour_length(node_feats, sol)
+        assert data[1] == length
+        # save solution data to folder
+        solution_data = {
+            "problem_name": problem_name,
+            "opt_tour_length": length,
+            "opt_tour": sol.tolist(),
+        }
+        with open(f"{instance_path}/solution.json", "w") as f:
+            # indent=2 is not needed but makes the file human-readable
+            json.dump(solution_data, f, indent=2)
---
+++ scripts/data_handling/sample_subset.py
@@ -0,0 +1,22 @@
+import numpy as np
+import argparse
+from main_code.utils.data.data_sets import DiskTSPTestSet
+from main_code.utils.data.utils import sample_and_save_subset
+
+
+if __name__ == "__main__":
+    # add argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset_path", type=str, default="./data/test_sets/fu_et_al_n_100_10000"
+    )
+    parser.add_argument(
+        "--save_path", type=str, default="./data/test_sets/fu_et_al_sample_n_100_128"
+    )
+    parser.add_argument("--sample_size", type=int, default=128)
+    opts = parser.parse_known_args()[0]
+    dataset_path = opts.dataset_path
+    dataset = DiskTSPTestSet(dataset_path, use_pomo_aug=False, sampling_steps=1)
+    sample_and_save_subset(
+        dataset, save_path=opts.save_path, sample_size=opts.sample_size
+    )
