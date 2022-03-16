ee97d150431991451ac2abdf25cf58e92022c812
dschaub95 <schaub.darius@gmail.com>
2022-03-16 13:23:41 +0100

    data: update script for convenient data copying

--- data/cp_test_sets.sh
+++ data/cp_test_sets.sh
@@ -5,3 +5,7 @@ cp -r  ../../pyconcorde/test_sets/uniform_n_200_128 test_sets/
 cp -r  ../../pyconcorde/test_sets/uniform_n_300_128 test_sets/
 cp -r  ../../pyconcorde/test_sets/uniform_n_400_128 test_sets/
 cp -r  ../../pyconcorde/test_sets/uniform_n_500_128 test_sets/
+
+cp -r  ../../pyconcorde/valid_sets/uniform_n_20_10000 valid_sets/
+cp -r  ../../pyconcorde/valid_sets/uniform_n_50_10000 valid_sets/
+cp -r  ../../pyconcorde/valid_sets/uniform_n_100_10000 valid_sets/
